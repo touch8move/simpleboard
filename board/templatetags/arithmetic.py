b64cf41db8e7bc71a831fc850e4583542c345798
Dongho <backack2@gmail.com>
2016-09-29 23:55:56 +0900

    날짜필터

--- board/templatetags/arithmetic.py
+++ board/templatetags/arithmetic.py
@@ -1,4 +1,5 @@
 from django import template
+from datetime import datetime, timedelta
 
 """It's sometimes really handy to be able to do basic arithmetic in
 Django templates.
@@ -24,3 +25,10 @@ def mul(value, arg):
 @register.filter
 def ip(value):
     return value.replace(value.split(".")[0],"*",1)
+
+
+@register.filter
+def new(value):
+    bftw = datetime.now() - timedelta(hours=12)
+    valuedt = datetime(year=value.year, month=value.month, day=value.day, hour=value.hour, minute=value.minute, second=value.second)
+    return True if datetime.now()< valuedt + timedelta(hours=12) else False 
